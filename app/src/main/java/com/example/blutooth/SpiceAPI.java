2bcfd96301e59f6a5cdbcdadfc41033f575bf723
MarcosBarros <marcosbarrosjr@gmail.com>
2020-07-15 20:48:28 -0400

    update

--- app/src/main/java/com/example/blutooth/SpiceAPI.java
+++ app/src/main/java/com/example/blutooth/SpiceAPI.java
@@ -10,10 +10,12 @@ package com.example.blutooth;
         import android.widget.EditText;
         import android.widget.LinearLayout;
         import android.widget.TextView;
+        import android.widget.Toast;
 
         import androidx.annotation.RequiresApi;
         import androidx.appcompat.app.AppCompatActivity;
 //import androidx.cardview.widget.CardView;
+        import androidx.cardview.widget.CardView;
         import androidx.constraintlayout.widget.ConstraintLayout;
 
 //import com.google.android.material.snackbar.Snackbar;
@@ -34,25 +36,48 @@ public class SpiceAPI extends AppCompatActivity {
     //Remember the CardView.
     private JSONObject apiResponse;
     private String q = "chicken";
-    private int from = 0, to = 19;
+    private int from = 0, to = 5;
+    private int delta = 0;
     private String nakedUrl = "https://api.edamam.com/search?q=" + q + "&app_id=3e6d9a08&app_key=b6723e25aa0fde680a04c6c7cf456de2&from=" + from + "&to=" + to;
 
-    Button send,back;
-    ConstraintLayout layout;
+    private CardView card1, card2, card3, card4, card5;
+    private TextView recipeTitle1, recipeTitle2, recipeTitle3, recipeTitle4, recipeTitle5;
+
+    Button send;
+    LinearLayout layout;
     TextView httpResponse;  //For testing purposes only
     EditText query;
+    ArrayList<String> spiceList = new ArrayList<>(4);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
+        setContentView(R.layout.activity_main);
+
+        //Simulate getting info from first page
+        spiceList.add("Salt");
+        spiceList.add("Black Pepper");
+        spiceList.add("Paprika");
+        spiceList.add("Oregano");
 
         send = (Button)findViewById(R.id.sendButton);
-        back = (Button)findViewById(R.id.backBtn);
+        layout = (LinearLayout)findViewById(R.id.linearLayout);
         httpResponse = new TextView(this);
         httpResponse.setText("Something for now.");
-
         query = (EditText)findViewById(R.id.query);
+
+        card1 = (CardView)findViewById(R.id.card1);
+        card2 = (CardView)findViewById(R.id.card2);
+        card3 = (CardView)findViewById(R.id.card3);
+        card4 = (CardView)findViewById(R.id.card4);
+        card5 = (CardView)findViewById(R.id.card5);
+
+        recipeTitle1 = (TextView)findViewById(R.id.recipeTitle1);
+        recipeTitle2 = (TextView)findViewById(R.id.recipeTitle2);
+        recipeTitle3 = (TextView)findViewById(R.id.recipeTitle3);
+        recipeTitle4 = (TextView)findViewById(R.id.recipeTitle4);
+        recipeTitle5 = (TextView)findViewById(R.id.recipeTitle5);
+
         /*
         try {
             startRequestThread(nakedUrl);
@@ -66,13 +91,7 @@ public class SpiceAPI extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }*/
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SpiceAPI.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,14 +112,19 @@ public class SpiceAPI extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
+                /*
+                Toast testMessage = Toast.makeText(getApplicationContext(), httpResponse.getText().toString(), Toast.LENGTH_LONG);
+                testMessage.show();*/
 
                 JSONArray recipes = null;
                 try {
                     recipes = getRecipesFromHits(apiResponse);
-                    //updateCardViews(recipes);
+                    updateCardViews(recipes);
                 }
                 catch(JSONException e) {
-                    ;
+                    e.printStackTrace();
+                    /*Toast jsonMessage = Toast.makeText(getApplicationContext(), "Got a JSON exception", Toast.LENGTH_LONG);
+                    jsonMessage.show();*/
                 }
             }
         });
@@ -160,9 +184,38 @@ public class SpiceAPI extends AppCompatActivity {
 
     private String[] getRecipeInfo(JSONObject recipe) throws JSONException {
         String[] info = new String[3];
-        info[0] = recipe.getString("label");    //Name of recipe
-        info[1] = recipe.getString("url");      //Recipe URL
-        info[2] = recipe.getString("image");    //Image URL
+
+        String wholeRecipe = recipe.toString();
+
+        boolean error[] = {false, false, false};
+        int uriIndex = wholeRecipe.indexOf("\"uri\"") + 7;
+        int labelIndex = wholeRecipe.indexOf("\"label\"") + 9;
+        int imageIndex = wholeRecipe.indexOf("\"image\"") + 9;
+        int sourceIndex = wholeRecipe.indexOf("\"source\"");
+
+        int indices[] = {uriIndex, labelIndex, imageIndex, sourceIndex};
+
+        if(uriIndex == -1 || labelIndex == -1) {
+            info[0] = "N/a";
+            error[0] = true;
+        }
+
+        if(labelIndex == -1 || imageIndex == -1) {
+            info[1] = "N/a";
+            error[1] = true;
+        }
+
+        if(imageIndex == -1 || sourceIndex == -1) {
+            info[2] = "N/a";
+            error[2] = true;
+        }
+
+        for(int i = 0; i < 3; i++) {
+            if(!error[i]) {
+                if(i != 2)  info[i] = wholeRecipe.substring(indices[i], indices[i+1]-9);
+                else info[i] = wholeRecipe.substring(indices[i], indices[i+1]-2);
+            }
+        }
 
         return info;
     }
@@ -177,8 +230,8 @@ public class SpiceAPI extends AppCompatActivity {
             JSONObject food = ingr.getJSONObject("food");
             JSONObject measure = ingr.getJSONObject("measure");
 
-            String ingrName = food.getString("label");
-            String unit = measure.getString("label");
+            String ingrName = getJSONString(food, "label", "END");
+            String unit = getJSONString(measure, "label", "END");
             int amt = ingr.getInt("quantity");  //this might give you issues due to implicit float to int conversion
 
             ingrList[0] = new Ingredient(ingrName, unit, amt);
@@ -186,15 +239,39 @@ public class SpiceAPI extends AppCompatActivity {
 
         return ingrList;
     }
-    /*
+
+    private static String getJSONString(JSONObject obj, String firstKey, String secondKey) throws JSONException{
+        boolean error;
+
+        String wholeObj = obj.toString();
+
+        int startIndex = wholeObj.indexOf(firstKey);
+        int endIndex = wholeObj.indexOf(secondKey);
+
+        if(secondKey.equals("END")) {
+            endIndex = wholeObj.indexOf("\"", startIndex + 1);
+        }
+
+        if(startIndex == -1 || endIndex == -1) {
+            error = true;
+            return "N/a";
+        }
+
+        startIndex += (firstKey.length() + 2);
+
+        return wholeObj.substring(startIndex, endIndex + 1);
+    }
+
     private void updateCardViews(JSONArray recipes) throws JSONException {
         if(recipes.equals(null)) {
+            Toast nullRecipe = Toast.makeText(getApplicationContext(), "Got null recipe JSON object", Toast.LENGTH_LONG);
+            nullRecipe.show();
             return;
         }
 
         ArrayList<String[]> recipeInfo = new ArrayList<>();
         ArrayList<Ingredient[]> ingredients = new ArrayList<>();
-        for(int i = 0; i < 20; i++) {
+        for(int i = 0; i < 5; i++) {
             JSONObject currentRecipe = recipes.getJSONObject(i);
 
             recipeInfo.add(getRecipeInfo(currentRecipe));
@@ -202,24 +279,19 @@ public class SpiceAPI extends AppCompatActivity {
         }
 
 
-        CardView[] cards = new CardView[20];
-        LinearLayout cardHolder = new LinearLayout(this);
-        for(int i = 0; i < 20; i++) {
-            cards[i] = new CardView(this);
-            //put formatting stuff here.
-
+        //CardView[] cards = new CardView[20];
+        TextView titles[] = {recipeTitle1, recipeTitle2, recipeTitle3, recipeTitle4, recipeTitle5};
+        TextView links[];
+        TextView ingredientList[];
+        for(int i = 0; i < 5; i++) {
             //populate cards
-            TextView recipeName = new TextView(this);
-            recipeName.setText(recipeInfo.get(i)[0]);
-            //add cards to layout
-            cards[i].setVisibility(View.VISIBLE);
-            cardHolder.addView(cards[i]);
-            ;
-            //layout.addView(recipeName);
+            titles[i].setText(recipeInfo.get(i)[1]);
+            Toast recipeChange = Toast.makeText(getApplicationContext(), "Should have changed the title by now", Toast.LENGTH_LONG);
+            recipeChange.show();
+
         }
-        layout.addView(cardHolder);
         return;
-    }*/
+    }
 
     private class Ingredient {
         private String name;
